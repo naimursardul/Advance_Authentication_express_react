1cd188d4707c025214bbb0a8614f11e53098c13a
Naimur <naimur30447@gmail.com>
2024-11-30 00:59:05 +0600

    init

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: process.env.BACK_END_URL }));
 
 app.use(
   session({
